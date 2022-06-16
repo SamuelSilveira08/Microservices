506f98d4f9152dc1a3490e4767982f2fb9521c10
SamuelSilveira08 <samuel.santossilveira08@gmail.com>
2022-06-16 16:01:23 -0300

    Implemented consumer, but now there's some database error going on. Will fix that later.

--- fraud/src/main/java/br/com/samuel/fraud/config/CostumerKafkaConsumerConfig.java
+++ fraud/src/main/java/br/com/samuel/fraud/config/CostumerKafkaConsumerConfig.java
@@ -12,7 +12,6 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.converter.JsonMessageConverter;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import br.com.samuel.fraud.domain.Customer;
