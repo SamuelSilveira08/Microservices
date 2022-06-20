8d6e37a91e0cd78c6fc8e48719508345d69f06a0
SamuelSilveira08 <samuel.santossilveira08@gmail.com>
2022-06-19 21:08:41 -0300

    Fixed the database related problem. It was being caused by me trying to save a customer into fraud_check table without customer having an id. Now I just gotta change the code so the id of customer x is always the same in the two databases (customer and fraud)

--- fraud/src/main/java/br/com/samuel/fraud/service/FraudCheckService.java
+++ fraud/src/main/java/br/com/samuel/fraud/service/FraudCheckService.java
@@ -1,6 +1,7 @@
 package br.com.samuel.fraud.service;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -35,8 +36,17 @@ public class FraudCheckService {
 		// Well, actually it's related to hibernate sequence. I don't know why, but it's not defining
 		// one id sequence for each table, but for the two tables in database
 		
-		customerRepository.save(customer);
-
+		customer = customerRepository.save(customer);
+		
+		System.out.println(customer.getId());
+		
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		fraudRepository.save(FraudCheckHistory.builder().isFraudster(false).customer(customer)
 				.createdAt(LocalDateTime.now()).build());
 
